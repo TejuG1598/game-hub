7cdbd3a567793bca435d483a1dbc7284c57f203b
TejuG1598 <tejaswisai.15@gmail.com>
2024-02-11 11:24:16 -0500

    Installed chakra UI

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### Getting Started
 - For styling using the chakra for guidlines refer https://chakra-ui.com/getting-started/vite-guide 
-- 
+- git log --oneline , this is used to see all the commits.
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@chakra-ui/react";
+import {  Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-      <Button colorScheme="blue">Button</Button>
+      <Grid templateAreas={`"nav   nav" 
+                            "aside main"`}>
+        <GridItem area = "nav" bg ="blueviolet">
+            NavBar
+        </GridItem>
+        <GridItem area = "aside" bg = "greenyellow">
+            Aside Panel 
+        </GridItem>
+        <GridItem area = "main" bg = "goldenrod">
+            Main Area
+        </GridItem>
+        
+      </Grid>
     </>
   );
 }
