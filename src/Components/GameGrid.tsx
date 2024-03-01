2e076034e87407bbe41f0b3929919e08a4a4e111
TejuG1598 <tejaswisai.15@gmail.com>
2024-03-01 17:42:23 -0500

    DisplayedGenresList

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -8,24 +8,27 @@ const GameGrid = () => {
   const { data, errors, isLoading } = useGames();
   const Skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (errors) return <Text>{errors}</Text>;
+
   return (
-    <>
-      {errors && <Text>{errors}</Text>}
-      <SimpleGrid spacing={10} padding={10} columns={{ sm: 1, md: 2, lg: 3 }}>
-        {isLoading &&
-          Skeletons.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSkeleton key={skeleton} />
-            </GameCardContainer>
-          ))}
-        {data &&
-          data.map((game) => (
-            <GameCardContainer>
-              <GameCard key={game.id} game={game} />
-            </GameCardContainer>
-          ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      padding="10px"
+      spacing={6}
+    >
+      {isLoading &&
+        Skeletons.map((skeleton) => (
+          <GameCardContainer  key={skeleton}>
+            <GameCardSkeleton />
+          </GameCardContainer>
+        ))}
+      {data &&
+        data.map((game) => (
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
+          </GameCardContainer>
+        ))}
+    </SimpleGrid>
   );
 };
 
